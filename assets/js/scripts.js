b242d779715f7f99438869993b705a616f2cde1b
Odinn H <info@odinh.design>
2019-10-02 22:30:46 +0200

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -98,7 +98,7 @@ function init()
         $('.remove-button').click(function(e)
         {
             var itemId = $(event.target).data("item");
-            console.log(data(item));
+            console.log(data("item"));
             if(disableMarkers.includes(itemId.toString()))
             {
                 disableMarkers = $.grep(disableMarkers, function(value) {
