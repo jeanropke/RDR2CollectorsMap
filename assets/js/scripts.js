150e85976d698a221b3203ad7cd599e46c69e23e
jeanropke <jeanropke@ymail.com>
2019-10-21 03:09:34 -0300

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -32,8 +32,8 @@ var lang;
 var languageData = [];
 
 var nazarLocations = [];
-var nazarCurrentLocation = 4;
-var nazarCurrentDate = '20th October';
+var nazarCurrentLocation = 8;
+var nazarCurrentDate = '21th October';
 
 var fastTravelData;
 
