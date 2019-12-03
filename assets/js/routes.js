0f914bd1476ac2af3fac73d8ca46e60e28b5d97a
jeanropke <jeanropke@ymail.com>
2019-12-03 13:55:20 -0300

    Update routes.js

--- assets/js/routes.js
+++ assets/js/routes.js
@@ -18,7 +18,8 @@ var Routes = {
           return;
 
         if (!collectedItems.includes(marker.text) && enabledCategories.includes(marker.category)
-        && uniqueSearchMarkers.includes(marker) && marker.tool <= parseInt(toolType)) {
+        && uniqueSearchMarkers.includes(marker) && !categoriesDisabledByDefault.includes(marker.subdata)
+        && marker.tool <= parseInt(toolType)) {
           var connection = [marker.lat, marker.lng];
           connections.push(connection);
         }
