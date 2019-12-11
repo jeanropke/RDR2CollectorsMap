6de26184c6c8232f7b83380cf072feb359d9d70e
jeanropke <jeanropke@ymail.com>
2019-12-11 03:48:56 -0300

    #442

--- assets/js/map.js
+++ assets/js/map.js
@@ -244,10 +244,10 @@ var MapBase = {
         _m.amount = 0;
 
       _m.canCollect = _m.amount < 10 && !_m.isCollected;
-      if ((_m.isCollected || _m.amount > 9) && _m.day == day) {
+      if ((_m.isCollected || _m.amount > 9) && (_m.day == day || _m.day.includes(day))) {
         $('[data-marker=' + _m.text + ']').css('opacity', '.35');
         $(`[data-type=${_m.text}]`).addClass('disabled');
-      } else if(_m.day == day) {
+      } else if((_m.day == day || _m.day.includes(day))) {
         $('[data-marker=' + _m.text + ']').css('opacity', '1');
         $(`[data-type=${_m.text}]`).removeClass('disabled');
       }
