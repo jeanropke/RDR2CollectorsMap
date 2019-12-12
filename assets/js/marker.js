406c76d76b2efba84741b592c779581d2e9bbde0
jeanropke <jeanropke@ymail.com>
2019-12-11 23:27:55 -0300

    commit

--- assets/js/marker.js
+++ assets/js/marker.js
@@ -12,5 +12,5 @@ var Marker = function(text, lat, lng, tool, day, category, subdata, video) {
   this.isVisible = enabledCategories.includes(category);
   this.amount = inventory[text] == null ? 0 : inventory[text].amount;
   this.isCollected = inventory[text] == null ? false : (inventory[text].isCollected);//collectedItems.includes(text);
-  this.canCollect = Inventory.isEnabled ? this.amount < Inventory.stackSize : !this.isCollected;
+  this.canCollect = Inventory.isEnabled ? this.amount < Inventory.stackSize && !this.isCollected : !this.isCollected;
 }
