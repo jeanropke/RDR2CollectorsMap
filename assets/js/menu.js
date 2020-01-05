c4f94aa150cc940fbb26ba741419d90de042d84c
jeanropke <jeanropke@ymail.com>
2020-01-04 23:07:23 -0300

    improved refresh menu

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -25,81 +25,73 @@ var Menu = {
 
 Menu.refreshMenu = function () {
   var weeklyItems = weeklySetData.sets[weeklySetData.current];
-  $.each(categories, function (key, category) {
 
-    $('.menu-hidden[data-type=' + category + ']').children('.collectible-wrapper').remove();
+  $.each(MapBase.markers, function (_key, marker) {
+    if(marker.day == Cycles.data.cycles[Cycles.data.current][marker.category]) {
+      if (marker.subdata) {
+        //This is for items with subdata to merge them
+        if ($(`.menu-hidden[data-type=${marker.category}]`).children(`[data-type=${marker.subdata}]`).length > 0)
+          return;
 
-    if (categoriesDisabledByDefault.includes(category))
-      $('.menu-option[data-type=' + category + ']').addClass('disabled');
+        var collectibleImage = null;
+        var collectibleName = null;
 
-    $.each(MapBase.markers, function (_key, marker) {
-      if (marker.day == Cycles.data.cycles[Cycles.data.current][category] && marker.category == category) {
-        if (marker.subdata) {
-          //This is for items with subdata to merge them
-          if ($(`.menu-hidden[data-type=${category}]`).children(`[data-type=${marker.subdata}]`).length > 0)
-            return;
+        if (marker.category == 'american_flowers') {
+          collectibleImage = $('<img>').attr('src', `./assets/images/icons/game/flower_${marker.subdata}.png`).addClass('collectible-icon');
+          collectibleName = Language.get(`flower_${marker.subdata}.name`);
+        } else if (marker.category == 'bird_eggs') {
+          collectibleImage = $('<img>').attr('src', `./assets/images/icons/game/egg_${marker.subdata}.png`).addClass('collectible-icon');
+          collectibleName = Language.get(`egg_${marker.subdata}.name`);
+        }
 
-          var collectibleImage = null;
-          var collectibleName = null;
+        var collectibleElement = $('<div>').addClass('collectible-wrapper').attr('data-type', marker.subdata);
+        var collectibleTextElement = $('<p>').addClass('collectible').text(collectibleName);
+        var collectibleCountElement = Inventory.isEnabled ? $('<small>').addClass('counter').text(marker.amount) : '';
 
-          if (marker.category == 'american_flowers') {
-            collectibleImage = $('<img>').attr('src', `./assets/images/icons/game/flower_${marker.subdata}.png`).addClass('collectible-icon');
-            collectibleName = Language.get(`flower_${marker.subdata}.name`);
-          } else if (marker.category == 'bird_eggs') {
-            collectibleImage = $('<img>').attr('src', `./assets/images/icons/game/egg_${marker.subdata}.png`).addClass('collectible-icon');
-            collectibleName = Language.get(`egg_${marker.subdata}.name`);
+        $('.menu-hidden[data-type=' + marker.category + ']').append(collectibleElement.append(collectibleImage).append(collectibleTextElement.append(collectibleCountElement)));
+
+        if (marker.amount == 10)
+          $(`[data-type=${marker.subdata}]`).addClass('disabled');
+
+        if (marker.lat.length == 0)
+          $(`[data-type=${marker.subdata}]`).addClass('not-found');
+
+        //set green color of weekly collection items (flowers and eggs)
+        for (var i = 0, weeklyItemsLength = weeklyItems.length; i < weeklyItemsLength; i++) {
+          if ((`flower_${marker.subdata}`) == weeklyItems[i].item || (`egg_${marker.subdata}`) == weeklyItems[i].item) {
+            $(`[data-type=${marker.subdata}]`).addClass('weekly-item');
           }
+        }
+      } else {
+        //All others items
+        var collectibleImage = null;
 
-          var collectibleElement = $('<div>').addClass('collectible-wrapper').attr('data-type', marker.subdata);
-          var collectibleTextElement = $('<p>').addClass('collectible').text(collectibleName);
-          var collectibleCountElement = Inventory.isEnabled ? $('<small>').addClass('counter').text(marker.amount) : '';
+        // Prevents 404 errors. If doing the if-statement the other way round, jQuery tries to load the images.
+        if (marker.category != 'random')
+          collectibleImage = $('<img>').attr('src', `./assets/images/icons/game/${marker.text}.png`).addClass('collectible-icon');
 
-          $('.menu-hidden[data-type=' + marker.category + ']').append(collectibleElement.append(collectibleImage).append(collectibleTextElement.append(collectibleCountElement)));
+        var collectibleElement = $('<div>').addClass('collectible-wrapper').attr('data-type', marker.text);
+        var collectibleTextElement = $('<p>').addClass('collectible').text(marker.title);
+        var collectibleCountElement = Inventory.isEnabled ? $('<small>').addClass('counter').text(marker.amount) : '';
 
-          if (marker.amount == 10)
-            $(`[data-type=${marker.subdata}]`).addClass('disabled');
+        $(`.menu-hidden[data-type=${marker.category}]`).append(collectibleElement.append(collectibleImage).append(collectibleTextElement.append(collectibleCountElement)));
 
-          if (marker.lat.length == 0)
-            $(`[data-type=${marker.subdata}]`).addClass('not-found');
+        if (marker.lat.length == 0)
+          $(`[data-type=${marker.text}]`).addClass('not-found');
 
-          //set green color of weekly collection items (flowers and eggs)
-          for (var i = 0, weeklyItemsLength = weeklyItems.length; i < weeklyItemsLength; i++) {
-            if ((`flower_${marker.subdata}`) == weeklyItems[i].item || (`egg_${marker.subdata}`) == weeklyItems[i].item) {
-              $(`[data-type=${marker.subdata}]`).addClass('weekly-item');
-            }
-          }
-        } else {
-          //All others items
-          var collectibleImage = null;
+        if (!marker.canCollect)
+          $(`[data-type=${marker.text}]`).addClass('disabled');
 
-          // Prevents 404 errors. If doing the if-statement the other way round, jQuery tries to load the images.
-          if (marker.category != 'random')
-            collectibleImage = $('<img>').attr('src', `./assets/images/icons/game/${marker.text}.png`).addClass('collectible-icon');
-
-          var collectibleElement = $('<div>').addClass('collectible-wrapper').attr('data-type', marker.text);
-          var collectibleTextElement = $('<p>').addClass('collectible').text(marker.title);
-          var collectibleCountElement = Inventory.isEnabled ? $('<small>').addClass('counter').text(marker.amount) : '';
-
-          $(`.menu-hidden[data-type=${category}]`).append(collectibleElement.append(collectibleImage).append(collectibleTextElement.append(collectibleCountElement)));
-
-          if (marker.lat.length == 0)
-            $(`[data-type=${marker.text}]`).addClass('not-found');
-
-          if (!marker.canCollect)
-            $(`[data-type=${marker.text}]`).addClass('disabled');
-
-          // set green color of weekly collection items (other items)
-          for (var i = 0, weeklyItemsLength = weeklyItems.length; i < weeklyItemsLength; i++) {
-            if ((marker.text) == weeklyItems[i].item) {
-              $(`[data-type=${marker.text}]`).addClass('weekly-item');
-            }
+        // set green color of weekly collection items (other items)
+        for (var i = 0, weeklyItemsLength = weeklyItems.length; i < weeklyItemsLength; i++) {
+          if ((marker.text) == weeklyItems[i].item) {
+            $(`[data-type=${marker.text}]`).addClass('weekly-item');
           }
         }
       }
-    });  
-    Menu.refreshTreasures();  
+    }
   });
-
+  Menu.refreshTreasures();  
   $.each(categoriesDisabledByDefault, function (key, value) {
     if (value.length > 0) {
       $('[data-type=' + value + ']').addClass('disabled');
