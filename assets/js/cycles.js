9dc900c2f38a3d5f2ad6071618c60b9e5563f5c6
MichalD96 <chemik.michal@gmail.com>
2020-01-10 22:35:22 +0100

    fancy feature, check if cycles are up to date, if not show alert

--- assets/js/cycles.js
+++ assets/js/cycles.js
@@ -23,5 +23,25 @@ var Cycles = {
                 .replace('{month}', Language.get(`menu.month.${_date[0]}`))
                 .replace('{day}', _date[1])
         );
+        return _date[1];
     }
 }
+
+// show alert when cycles are not up to date
+setInterval(function () {
+    var newDate = new Date(),
+        day = newDate.getUTCDate(),
+        hour = newDate.getUTCHours(),
+        minute = newDate.getUTCMinutes();
+    
+    if (hour < 1 && minute <= 20)
+        $('.map-alert').css('opacity', '1');
+    
+    if (day != Cycles.setLocaleDate()) {
+        $('.map-alert').css('opacity', '1');
+        return;
+    }
+    else
+        $('.map-alert').css('opacity', '0');
+    
+}, 1000 * 60);
