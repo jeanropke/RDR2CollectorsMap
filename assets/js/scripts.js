cdcb7fba6006016ad556e02e703cc86ac7b05f2d
Michal__d <chemik.michal@gmail.com>
2020-01-13 20:53:20 +0100

    back to first version bc works better

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -454,13 +454,12 @@ $('.collection-reset').on('click', function (e) {
     value.isCollected = false;
     value.canCollect = true;
 
-    if (Inventory.isMenuUpdateEnabled) {
       if (value.subdata)
-        Inventory.changeMarkerAmount(value.subdata, -1);
-      else
-        Inventory.changeMarkerAmount(value.text, -1);
-    }
+        Inventory.changeMarkerAmount(value.subdata, (Inventory.isMenuUpdateEnabled ? -1 : 0));
 
+      else
+        Inventory.changeMarkerAmount(value.text, (Inventory.isMenuUpdateEnabled ? -1 : 0));
+    
     $(this).removeClass('disabled');
   });
   MapBase.save();
