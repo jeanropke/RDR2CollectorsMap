d73919428a01b8e676c5ff5797cabbab0b57f6b0
Michal__d <chemik.michal@gmail.com>
2020-02-12 07:10:27 +0100

    forgot about UTC

--- assets/js/cycles.js
+++ assets/js/cycles.js
@@ -13,8 +13,8 @@ var Cycles = {
   getTodayCycle: function () {
     var utcDate = new Date();
     var utcYesterdayDate = new Date();
-    utcDate.setDate(utcDate.getDate() + Cycles.offset);
-    utcYesterdayDate.setDate(utcYesterdayDate.getDate() - 1 + Cycles.offset);
+    utcDate.setDate(utcDate.getUTCDate() + Cycles.offset);
+    utcYesterdayDate.setDate(utcYesterdayDate.getUTCDate() - 1 + Cycles.offset);
 
     var yesterday_data = Cycles.data.filter(_c => { return _c.date === MapBase.formatDate(`${utcYesterdayDate.getUTCFullYear()}/${(utcYesterdayDate.getUTCMonth() + 1)}/${utcYesterdayDate.getUTCDate()}`).toLowerCase() })[0];
     var _data = Cycles.data.filter(_c => { return _c.date === MapBase.formatDate(`${utcDate.getUTCFullYear()}/${(utcDate.getUTCMonth() + 1)}/${utcDate.getUTCDate()}`).toLowerCase() })[0];
