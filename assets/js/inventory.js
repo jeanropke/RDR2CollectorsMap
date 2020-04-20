674db508794a85e3c7609e46aba86eab27621d39
Michal__d <chemik.michal@gmail.com>
2020-04-20 18:39:44 +0200

    Change String.includesOneOf() to Array.includes()

--- assets/js/inventory.js
+++ assets/js/inventory.js
@@ -206,7 +206,7 @@ var Inventory = {
         $(`[data-type=${marker.legacyItemId}]`).removeClass('disabled');
       }
 
-      if (marker.isCurrent && marker.category.includesOneOf('egg', 'flower')) {
+      if (marker.isCurrent && ['egg', 'flower'].includes(marker.category)) {
         $(`[data-type=${marker.legacyItemId}]`).toggleClass('disabled',
           sameItemMarkers.filter(m => m.cycleName === marker.cycleName).every(m => !m.canCollect));
       }
