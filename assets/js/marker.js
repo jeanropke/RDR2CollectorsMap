80923cc1c6a02807081e625a515d9bcde217014f
Michal__d <chemik.michal@gmail.com>
2020-07-30 15:26:22 +0200

    Disable mark as important on random items

--- assets/js/marker.js
+++ assets/js/marker.js
@@ -304,7 +304,8 @@ class Marker {
     }
     if (!Settings.isDebugEnabled) snippet.find('.popupContentDebug').hide();
     if (!this.video) snippet.find('[data-text="map.video"]').parent().hide();
-    if (['flower_agarita', 'flower_blood_flower'].includes(this.itemId)) {
+    if (['flower_agarita', 'flower_blood_flower'].includes(this.itemId) ||
+    ['random', 'fossils_random', 'heirlooms_random', 'jewelry_random', 'coin', 'arrowhead'].includes(this.category)) {
       snippet.find('[data-text="map.mark_important"]').parent().hide();
     }
     const inventoryButtons = snippet.find('.marker-popup-buttons')
