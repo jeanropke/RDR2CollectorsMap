dbff8e2954b997ad587f3cff1f8bca6c3bd094d4
Michal__d <53737762+MichalD96@users.noreply.github.com>
2021-03-10 21:25:42 +0100

    Sort alphabetically all marker colors in every lang.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -943,7 +943,11 @@ $('#loot-table-modal').on('show.bs.modal', function (event) {
 
 
 $('#open-custom-marker-color-modal').on('click', event => {
-  const markerColors = ['aquagreen', 'beige', 'black', 'blue', 'brown', 'cadetblue', 'darkblue', 'darkgreen', 'darkorange', 'darkpurple', 'darkred', 'gray', 'green', 'lightblue', 'lightgray', 'lightgreen', 'lightorange', 'lightred', 'orange', 'pink', 'purple', 'red', 'white', 'yellow'];
+  const markerColors = ['aquagreen', 'beige', 'black', 'blue', 'brown', 'cadetblue', 'darkblue', 'darkgreen', 'darkorange', 'darkpurple', 'darkred', 'gray', 'green', 'lightblue', 'lightgray', 'lightgreen', 'lightorange', 'lightred', 'orange', 'pink', 'purple', 'red', 'white', 'yellow']
+    .sort((...args) => {
+      const [a, b] = args.map(color => Language.get(`map.user_pins.color.${color}`));
+      return a.localeCompare(b, Settings.language, { sensitivity: 'base' });
+    });
   const baseColors = { arrowhead: 'purple', bottle: 'brown', coin: 'darkorange', egg: 'white', flower: 'red', fossils_random: 'darkgreen', cups: 'blue', swords: 'blue', wands: 'blue', pentacles: 'blue', jewelry_random: 'yellow', bracelet: 'yellow', necklace: 'yellow', ring: 'yellow', earring: 'yellow', heirlooms: 'pink', random: 'lightgray', random_spot_metal: 'lightgray', random_spot_shovel: 'lightgray' };
   const randomCategories = ['random_spot_metal', 'random_spot_shovel']; // divide random spots to metal detector and shovel
   const itemCollections = Collection.collections;
