2617291fe82a32648617d0b41ba19606490b5dab
Senexis <timoviveen@gmail.com>
2021-04-17 15:50:49 +0200

    Tippy under menu, temp. qmode marker fix.

--- assets/js/map.js
+++ assets/js/map.js
@@ -618,6 +618,7 @@ const MapBase = {
   },
 
   yieldingLoop: function (count, chunksize, callback, finished) {
+    if (MapBase.isPreviewMode) chunksize = count;
     let i = 0;
     (function chunk() {
       const end = Math.min(i + chunksize, count);
@@ -652,6 +653,7 @@ const MapBase = {
         placement: 'right',
         arrow: false,
         distance: 0,
+        zIndex: 910,
         content(ref) {
           return ref.getAttribute('data-tippy');
         },
