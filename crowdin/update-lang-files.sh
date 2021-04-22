d34004c0e24f6c7b9f02e787b38a5f80c9de14cf
Senexis <timoviveen@gmail.com>
2021-04-22 23:14:51 +0200

    This should work to bail early.

--- crowdin/update-lang-files.sh
+++ crowdin/update-lang-files.sh
@@ -10,11 +10,13 @@ cd ..
   crowdin upload sources -b Main
   crowdin upload translations -b Main
 
-  # Cont files.
-  node "crowdin/index.js"
+  if [[ $(git status --porcelain) ]]; then
+    # Cont files.
+    node "crowdin/index.js"
 
-  # Commit.
-  git add .
-  git commit -m "Automatic language update."
-  git push
+    # Commit.
+    git add .
+    git commit -m "Automatic language update."
+    git push
+  fi
 } >"logs/lang-$(date +"%Y-%m-%d_%H-%M").log" 2>&1
