961b5e745f26c8de7344745f8f949c5919974eac
gebederry <zhaodejun1@gmail.com>
2024-09-05 11:05:43 +0800

    fix: The arrow indicator doesn't change when scroller reachs bottom This issue was introduced in PR https://github.com/jeanropke/RDR2CollectorsMap/pull/1251

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -414,18 +414,31 @@ function clockTick() {
 - please move them out of here to their respective owners
 */
 const sideMenu = document.querySelector('.side-menu');
+const scrollerLineTop = document.querySelector('.scroller-line-tp');
+const scrollerArrowTop = document.querySelector('.scroller-arrow-tp');
+const scrollerLineBottom = document.querySelector('.scroller-line-bt');
+const scrollerArrowBottom = document.querySelector('.scroller-arrow-bt');
 const backToTop = document.getElementById('back-to-top');
 const draggableBackToTop = draggify(backToTop, { storageKey: 'rdr2collector.backToTopPosition' });
+let wasAtTop = true;
+let wasAtBottom = false;
 let lastScrollY = sideMenu.scrollTop;
 
 sideMenu.addEventListener('scroll', function () {
   // These are not equality checks because of mobile weirdness.
   const atTop = this.scrollTop <= 0;
-  const atBottom = this.scrollTop + this.clientHeight >= this.scrollHeight;
-  document.querySelector('.scroller-line-tp').style.display = atTop ? '' : 'none';
-  document.querySelector('.scroller-arrow-tp').style.display = atTop ? 'none' : '';
-  document.querySelector('.scroller-line-bt').style.display = atBottom ? '' : 'none';
-  document.querySelector('.scroller-arrow-bt').style.display = atBottom ? 'none' : '';
+  const atBottom = Math.abs(this.scrollHeight - this.scrollTop - this.clientHeight) < 1;
+
+  if (atTop !== wasAtTop) {
+    scrollerLineTop.style.display = atTop ? '' : 'none';
+    scrollerArrowTop.style.display = atTop ? 'none' : '';
+    wasAtTop = atTop;
+  }
+  if (atBottom !== wasAtBottom) {
+    scrollerLineBottom.style.display = atBottom ? '' : 'none';
+    scrollerArrowBottom.style.display = atBottom ? 'none' : '';
+    wasAtBottom = atBottom;
+  }
 
   if (this.scrollTop !== 0 && this.scrollTop < lastScrollY) {
     backToTop.classList.add('is-visible');
@@ -1533,4 +1546,14 @@ function draggify(el, { storageKey }) {
     getDistanceMoved: () =>
       Math.sqrt((currentX - initialX) ** 2 + (currentY - initialY) ** 2)
   };
+}
+
+function toggleVisibility(el, visible) {
+  if (visible) {
+    el.style.visibility = 'visible';
+    el.style.opacity = '1';
+  } else {
+    el.style.opacity = '0';
+    el.style.visibility = 'hidden';
+  }
 }
